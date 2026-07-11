c4ff6dc584ecaa9f51b39b15801dcf271e5ecb3a
cejeh001 <cejeh001@gold.ac.uk>
2026-07-11 02:13:20 +0100

    sdfsdf

--- insert_test_data.sql
+++
@@ -1,9 +0,0 @@
-USE health;
-
-INSERT INTO users (username, password) VALUES
-('gold', 'smiths123ABC$');
-
-INSERT INTO foods (name, calories, protein, carbs, fats) VALUES
-('Chicken Breast', 165, 31, 0, 3.6),
-('Brown Rice', 216, 5, 45, 1.8),
-('Avocado', 240, 3, 12, 22);
